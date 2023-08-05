b326e4283bf5cc53d9ac2f443a952c9b10436580
juansantoss <juanjesussantos@hotmail.com>
2023-08-05 17:43:34 -0300

    Novo atualizaçaõ

---
+++ Views/Student/Notas.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NasaSchool.Views.Student
+{
+    public class Notas
+    {
+        fawf
+            wa
+            faw
+            faw
+            fa
+            w
+    }
+}
